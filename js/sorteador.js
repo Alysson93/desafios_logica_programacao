83e7f01abd468c515a6fd1e34574f157aae53bf4
Alysson93 <alyssonpereiraassuncao@gmail.com>
2025-04-27 15:18:56 -0300

    sorteador completo

--- js/sorteador.js
+++ js/sorteador.js
@@ -3,24 +3,39 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
     let numeros = [];
-    let result = 'Números sorteados: ';
     for (let i = 0; i < quantidade; i++) {
-        let valor = Math.floor(Math.random() * (ate - de + 1)) + de;
+        let valor;
+        do {
+            valor = Math.floor(Math.random() * (ate - de + 1)) + de;
+        } while(numeros.includes(valor) || numeros.length > quantidade);
         numeros.push(valor);
-        result += `${valor} `;
     }
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = result;
-    let btn_sortear = document.getElementById('btn-sortear');
-    let btn_reiniciar = document.getElementById('btn-reiniciar');
-    btn_sortear.setAttribute('disabled');
-    btn_reiniciar.removeAttribute('disabled');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numeros}</label>`
+    alterarBotao();
 }
 
 function reiniciar() {
-    let quantidade = document.getElementById('quantidade');
-    let de = document.getElementById('de');
-    let ate = document.getElementById('ate');
-    let resultado = document.getElementById('resultado');
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarBotao();
+}
 
+function alterarBotao() {
+    let sortear = document.getElementById('btn-sortear');
+    let reiniciar = document.getElementById('btn-reiniciar');
+    if (sortear.classList.contains('container__botao')) {
+        console.log('ok');
+        sortear.classList.remove('container__botao');
+        sortear.classList.add('container__botao-desabilitado');
+        reiniciar.classList.remove('container__botao-desabilitado');
+        reiniciar.classList.add('container__botao');
+    } else {
+        sortear.classList.remove('container__botao-desabilitado');
+        sortear.classList.add('container__botao');
+        reiniciar.classList.remove('container__botao');
+        reiniciar.classList.add('container__botao-desabilitado');
+    }
 }
